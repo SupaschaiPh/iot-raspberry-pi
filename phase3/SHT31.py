1d0b46718d352f902541527c8bbfa8cfeaabe914
Supass Ph. <112678332+SupaschaiPh@users.noreply.github.com>
2024-08-08 16:56:22 +0700

    !! SHT31.py

--- phase3/SHT31.py
+++ phase3/SHT31.py
@@ -1,89 +1,44 @@
+# Distributed with a free-will license.
+# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+# SHT31
+# This code is designed to work with the SHT31_I2CS I2C Mini Module available from ControlEverything.com.
+# https://www.controleverything.com/content/Humidity?sku=SHT31_I2CS#tabs-0-product_tabset-2
+
 from smbus3 import SMBus
 import time
+def readBus():
+    # Get I2C bus
+    bus = SMBus(1)
 
-class LCD:
-    def __init__(self, pi_rev = 2, i2c_addr = 0x3F, backlight = True):
+    # SHT31 address, 0x44(68)
+    # Send measurement command, 0x2C(44)
+    #		0x06(06)	High repeatability measurement
+    bus.write_i2c_block_data(0x44, 0x2C, [0x06])
 
-        # device constants
-        self.I2C_ADDR  = i2c_addr
-        self.LCD_WIDTH = 16   # Max. characters per line
+    time.sleep(0.5)
 
-        self.LCD_CHR = 1 # Mode - Sending data
-        self.LCD_CMD = 0 # Mode - Sending command
+    # SHT31 address, 0x44(68)
+    # Read data back from 0x00(00), 6 bytes
+    # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+    data = bus.read_i2c_block_data(0x44, 0x00, 6)
 
-        self.LCD_LINE_1 = 0x80 # LCD RAM addr for line one
-        self.LCD_LINE_2 = 0xC0 # LCD RAM addr for line two
+    # Convert the data
+    temp = data[0] * 256 + data[1]
+    cTemp = -45 + (175 * temp / 65535.0)
+    fTemp = -49 + (315 * temp / 65535.0)
+    humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
 
-        if backlight:
-            # on
-            self.LCD_BACKLIGHT  = 0x08
-        else:
-            # off
-            self.LCD_BACKLIGHT = 0x00
+    # Output data to screen
+    #print("Temperature in Celsius is : %.2f C" %cTemp)
+    #print("Temperature in Fahrenheit is : %.2f F" %fTemp)
+    #print("Relative Humidity is : %.2f %%RH" %humidity)
+    return cTemp,fTemp,humidity
 
-        self.ENABLE = 0b00000100 # Enable bit
+if __name__ == "__main__":
+    while True:
+        cTemp,fTemp,humidity =  readBus()
+        print("Temperature in Celsius is : %.2f C" %cTemp)
+        print("Temperature in Fahrenheit is : %.2f F" %fTemp)
+        print("Relative Humidity is : %.2f %%RH" %humidity)
+        time.sleep(1)
 
-        # Timing constants
-        self.E_PULSE = 0.0005
-        self.E_DELAY = 0.0005
-
-        # Open I2C interface
-        if pi_rev == 2:
-            # Rev 2 Pi uses 1
-            self.bus = SMBus(1)
-        elif pi_rev == 1:
-            # Rev 1 Pi uses 0
-            self.bus = SMBus(0)
-        else:
-            raise ValueError('pi_rev param must be 1 or 2')
-
-        # Initialise display
-        self.lcd_byte(0x33, self.LCD_CMD) # 110011 Initialise
-        self.lcd_byte(0x32, self.LCD_CMD) # 110010 Initialise
-        self.lcd_byte(0x06, self.LCD_CMD) # 000110 Cursor move direction
-        self.lcd_byte(0x0C, self.LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-        self.lcd_byte(0x28, self.LCD_CMD) # 101000 Data length, number of lines, font size
-        self.lcd_byte(0x01, self.LCD_CMD) # 000001 Clear display
-
-    def lcd_byte(self, bits, mode):
-        # Send byte to data pins
-        # bits = data
-        # mode = 1 for data, 0 for command
-
-        bits_high = mode | (bits & 0xF0) | self.LCD_BACKLIGHT
-        bits_low = mode | ((bits<<4) & 0xF0) | self.LCD_BACKLIGHT
-
-        # High bits
-        self.bus.write_byte(self.I2C_ADDR, bits_high)
-        self.toggle_enable(bits_high)
-
-        # Low bits
-        self.bus.write_byte(self.I2C_ADDR, bits_low)
-        self.toggle_enable(bits_low)
-
-    def toggle_enable(self, bits):
-        time.sleep(self.E_DELAY)
-        self.bus.write_byte(self.I2C_ADDR, (bits | self.ENABLE))
-        time.sleep(self.E_PULSE)
-        self.bus.write_byte(self.I2C_ADDR,(bits & ~self.ENABLE))
-        time.sleep(self.E_DELAY)
-
-    def message(self, string, line = 1):
-        # display message string on LCD line 1 or 2
-        if line == 1:
-            lcd_line = self.LCD_LINE_1
-        elif line == 2:
-            lcd_line = self.LCD_LINE_2
-        else:
-            raise ValueError('line number must be 1 or 2')
-
-        string = string.ljust(self.LCD_WIDTH," ")
-
-        self.lcd_byte(lcd_line, self.LCD_CMD)
-
-        for i in range(self.LCD_WIDTH):
-            self.lcd_byte(ord(string[i]), self.LCD_CHR)
-
-    def clear(self):
-        # clear LCD display
-        self.lcd_byte(0x01, self.LCD_CMD)
