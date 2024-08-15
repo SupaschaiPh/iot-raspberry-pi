e8e0a511eb429bb401d76ad152cf6d6cc82e3d83
Supass Ph. <112678332+SupaschaiPh@users.noreply.github.com>
2024-08-15 17:10:06 +0700

    Update ws_server.py

--- phase4/ws_server.py
+++ phase4/ws_server.py
@@ -1,21 +1,20 @@
 
 import asyncio
 import websockets
-from temp import ReadChannel,CalcTempMCPDegC
 
+connected_clients = set()
 
-async def readTempToClient(websocket):
-        while True:
-                await websocket.send(str(CalcTempMCPDegC(ReadChannel(1))[2]))
-                await asyncio.sleep(1)
-
-async def echo(websocket, path):
-        async for message in websocket:
-                print(f"Receive from client : {message}")
-                await asyncio.gather(readTempToClient(websocket))#websocket.send(f"{message}"))
+async def handle_client(websocket, path):
+        connected_clients.add(websocket)
+        try:
+                async for message in websocket:
+                        print(f"Receive message : {message}")
+                        await asyncio.gather(*[client.send(message) for client in connected_clients])
+        finally:
+                connected_clients.remove(websocket)
 
 async def main():
-        server = await websockets.serve(echo,"0.0.0.0",8765)
+        server = await websockets.serve(handle_client,"0.0.0.0",8765)
         print("Server Start,waiting for communication...")
         await server.wait_closed()
 
